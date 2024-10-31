a58d2b3f4810cf72920aad9b8589e756cbae4762
KluEvo <30702547+KluEvo@users.noreply.github.com>
2024-10-30 19:50:19 -0500

    updated find special awards

--- helperFuncs.py
+++ helperFuncs.py
@@ -108,6 +108,7 @@ def remove_symbols(a_tweet):
 def find_special_awards(tweets):
     regexp = "(?<=the)(.*)(?=award)"
     specials = []
+
     for i in tweets:
         tweet = remove_symbols(i['text'])
         result = re.search(regexp, tweet)
@@ -117,16 +118,16 @@ def find_special_awards(tweets):
                 continue
             specials.append(tweet[start:end])
     specials = list(Counter(specials).most_common(100))
-    special_awards =  specials[0:5]
+    special_awards = specials[0:5]
 
+    
     awards = []
-    special_awards = find_special_awards(tweets)
     for i in special_awards:
         cont = True
-        if fuzz.ratio(i.lower(), "golden globes") > 75:
+        if fuzz.ratio(i[0].lower(), "golden globes") > 70 or "golden globe" in i[0].lower():
             cont = False
         if cont:
             itxt = i[0].title()
-            awards.append(itxt)
+            awards.append(itxt.strip())
 
     return list(set(awards))
